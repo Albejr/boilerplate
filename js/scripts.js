ad4265e1482f12ec33692b8f1ffde759ee008233
albejr <jr.albertino@outlook.com>
2020-03-29 12:13:00 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+console.log('-------------------------------');
+console.log('     _    _ _');
+console.log('    / \\  | | |__   ___');
+console.log('   / _ \\ | |  _ \\ / _ \\ ');
+console.log('  / ___ \\| | |_) |  __/');
+console.log(' /_/   \\_\\_|_.__/ \\___|');
+console.log(' ');
+console.log('-------------------------------');
 
 $(document).ready(function(){
 
